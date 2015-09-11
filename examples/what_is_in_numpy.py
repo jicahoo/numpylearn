830c7e1b68c5eabf1b15bad84f4661667b62f91a
Jichao Zhang <jicahoo@163.com>
2015-09-11 10:32:18 +0000

    Add stats logic of numpy

--- examples/what_is_in_numpy.py
+++ examples/what_is_in_numpy.py
@@ -1,4 +1,26 @@
 import numpy
+import operator
+import types
+import pprint
 
 #Size of things from numpy
+seq=0
+
+seq+=1
+print "\n%s. Total number:" % (seq)
 print "The total number of things from numpy is %s module." % len(dir(numpy))
+
+#Get stats info of attrs of numpy 
+stats_of_type = {}
+for item in dir(numpy):
+    #print type(eval('numpy.%s' % item))
+    attr_of_numpy = eval('numpy.%s' % item)
+    attr_type = type(attr_of_numpy)
+    if str(attr_type) in stats_of_type:
+        stats_of_type[str(attr_type)] +=1
+    else:
+        stats_of_type[str(attr_type)] = 1
+
+seq += 1
+print "\n%s. Stats of numpy:" % (seq)
+pprint.pprint(sorted( stats_of_type.items(), key = operator.itemgetter(1), reverse = True))
